da801eb8ef0515f6a790159ee3e801da838c172f
Toworkit <1211126374@qq.com>
2018-06-23 13:00:34 +0800

    flume对接kafka

---
+++ sparktrain/src/main/generate_log.py
@@ -0,0 +1,36 @@
+#coding=UTF-8
+
+import random
+
+# 访问路径
+url_paths = [
+  "class/112.html",
+  "class/128.html",
+  "class/145.html",
+  "class/146.html",
+  "class/131.html",
+  "class/130.html",
+  "learn/821",
+  "course/list"
+]
+
+# ip
+ip_slices = [132, 156, 124, 10, 29, 167, 143, 187, 30, 46, 55, 63, 72, 87, 98]
+
+# 随机路径
+def sample_url():
+  # sample(seq, n) 从序列seq中选择n个随机且独立的元素
+  return random.sample(url_paths, 1)[0]
+
+# 随机ip
+
+
+# 日志生成
+def generate_log(count = 10):
+  while count >= 1:
+    query_log = "{url}".format(url = sample_url()) 
+    print(query_log)
+    count -= 1
+
+if __name__ == '__main__':
+  generate_log()
