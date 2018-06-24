d3696a1d3d11af28730464b784b48b3a9c8b0ea1
Toworkit <1211126374@qq.com>
2018-06-25 01:34:33 +0800

    数据清洗

--- spark/src/main/scala/com/imooc/spark/project/ImoocStatStreamingApp.scala
+++ spark/src/main/scala/com/imooc/spark/project/ImoocStatStreamingApp.scala
@@ -1,6 +1,5 @@
 package com.imooc.spark.project
 
-import kafka.serializer.StringDecoder
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
@@ -13,14 +12,26 @@ object ImoocStatStreamingApp {
     val conf = new SparkConf().setAppName("ImoocStatStreamingApp").setMaster("local[2]")
     val ssc = new StreamingContext(conf,Seconds(60))
 
-    //指定Topic信息：从streamingtopic的topic中，每次接受一条消息
+    // 指定Topic信息：从streamingtopic的topic中，每次接受一条消息
     val topic = Map("streamingtopic" -> 1)
 
-    //创建Kafka输入流，基于Receiver方式，链接到ZK
+    // 创建Kafka输入流，基于Receiver方式，链接到ZK
     val messages = KafkaUtils.createStream(ssc,"192.168.187.116:2181","test",topic)
 
-    //接受数据，并处理
-    messages.map(_._2).print()
+    // 打印数据
+    // messages.map(_._2).print()
+
+    // 数据清洗
+    // 124.63.98.156	2018-06-24 12:11:01	"GET /class/112.html HTTP/1.1"	500	https://www.sogou.com/web?query=Spark SQL实战
+    val logs = messages.map(_._2)
+    val cleanData = logs.map(line => {
+      val infos = line.split("\t")
+      val url = infos(2).split(" ")(1)
+      // 课程id
+      val courseId = 0
+      // 清洗 必须是以 /class 开头的数据，其他数据忽略
+    })
+
 
     ssc.start()
     ssc.awaitTermination()
